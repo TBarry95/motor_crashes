33c48f42d219b280aa0fe7df79ef48112c118b44
Tiernan Barry <tiernan.b95@gmail.com>
2022-02-09 22:32:22 +0000

    app

--- ShinyApps/MotorCrashAnalytics/app.r
+++ ShinyApps/MotorCrashAnalytics/app.r
@@ -53,7 +53,7 @@ DefineUserInterface <- function(){
         shinydashboard::tabItems(
           
           ##########################################
-          # Explore :
+          # DQ :
           ##########################################
           
           shinydashboard::tabItem(
@@ -61,13 +61,43 @@ DefineUserInterface <- function(){
             plotly::plotlyOutput("dq_fig1")
           ),
           
+          
+          ##########################################
+          # Explore :
+          ##########################################
+          
           shinydashboard::tabItem(
             tabName="eda1",
             plotly::plotlyOutput("ts_fig1")
+            
+            # shiny::mainPanel(
+            #   shiny::tabsetPanel(
+            #     shiny::tabPanel(
+            #       tabName="Crash Time Series",
+            #       plotly::plotlyOutput("ts_fig1")
+            #     ),
+            #     shiny::tabPanel(
+            #       tabName="Crash Map",
+            #       plotly::plotlyOutput("")
+            #     )
+            # 
+            #   )
+            # )
+            
           ),
+          
+          ##########################################
+          # Hyp :
+          ##########################################
           
           shinydashboard::tabItem(
             tabName="covid_hyp",
+            
+            tags$h2("Hypothesis: COVID-19 has caused the number of car crashes to fall in New York"),
+            tags$h4("H0: There is no difference in crash volumes between before and after COVID-19"),
+            tags$h4("H1: There is a difference in crash volumes between before and after COVID-19"),
+            br(),
+            
             column(
               width=6,
               plotly::plotlyOutput("hist_fig1")
@@ -75,6 +105,13 @@ DefineUserInterface <- function(){
             column(
               width=6,
               plotly::plotlyOutput("hist_fig2")
+            ),
+            fluidRow(
+              column(
+                width=12,
+                shiny::textOutput("shapiro_wilk"),
+                shiny::textOutput("mean_test")
+              )
             )
           )
         )
@@ -102,7 +139,10 @@ DefineServer <- function(input, output){
   hist_fig1 <- readRDS("/home/rstudio/motor_crashes/Output/hist_hyp_before.rds")
   hist_fig2 <- readRDS("/home/rstudio/motor_crashes/Output/hist_hyp_after.rds")
   
+  aft_sample1 <- readRDS("/home/rstudio/motor_crashes/Output/hyp_sample_aft.rds")
+  bf_sample1 <- readRDS("/home/rstudio/motor_crashes/Output/hyp_sample_bf.rds")
   
+
   #########################################
   # DQ tab
   #########################################
@@ -131,6 +171,13 @@ DefineServer <- function(input, output){
     print(hist_fig2)
   })
   
+  output$shapiro_wilk <- shiny::renderText({
+    "Both samples did not display a normal distribution, as per the Shapiro Wilk Test for normality. "
+  })
+  
+  output$mean_test <- shiny::renderText({
+    "Using the Wilcoxon Rank Sum test, the Null Hypothesis was Rejected (P-Value = 0)"
+  })
   
 }
 
--- get_data.r
+++ get_data.r
@@ -7,6 +7,7 @@ library("aws.s3")
 library("utils")
 library("plotly")
 #library("naniar")
+library("rpivotTable")
 
 ############################################################
 # Get data
@@ -35,6 +36,10 @@ print(head(motor_data))
 
 nrow(motor_data)
 length(unique(motor_data$COLLISION_ID))
+
+motor_data$TOTAL_KILLED <- rowSums(motor_data[, c("NUMBER.OF.PERSONS.KILLED", "NUMBER.OF.PEDESTRIANS.KILLED", "NUMBER.OF.CYCLIST.KILLED", "NUMBER.OF.MOTORIST.KILLED" )])
+motor_data$TOTAL_INJURED <- rowSums(motor_data[, c("NUMBER.OF.PERSONS.INJURED", "NUMBER.OF.PEDESTRIANS.INJURED", "NUMBER.OF.CYCLIST.INJURED", "NUMBER.OF.MOTORIST.INJURED" )])
+
 
 ############################################################
 # Explore data:
@@ -79,16 +84,33 @@ saveRDS(dq_report_fig, "/home/rstudio/motor_crashes/Output/dq_report_fig1.rds")
 # - Time series of crashes by date
 ts_data <- unique(motor_data[, c("CRASH.DATE", "COLLISION_ID")])
 ts_data_sum <- data.frame(table(ts_data$CRASH.DATE))
+
+ts_data2 <- unique(motor_data[, c("CRASH.DATE", "TOTAL_KILLED")])
+ts_data3 <- unique(motor_data[, c("CRASH.DATE", "TOTAL_INJURED")])
+
+ts_data_sum2 <- data.frame(table(ts_data2$CRASH.DATE))
+ts_data_sum3 <- data.frame(table(ts_data3$CRASH.DATE))
+
+
 names(ts_data_sum) <- c("DATE", "CRASHES")
+names(ts_data_sum2) <- c("DATE", "KILLED")
+names(ts_data_sum3) <- c("DATE", "INJURED")
+
+ts_data_sum <- merge(ts_data_sum,ts_data_sum2,by="DATE",all.x=TRUE)
+ts_data_sum <- merge(ts_data_sum,ts_data_sum3,by="DATE",all.x=TRUE)
+ts_data_sum[is.na(ts_data_sum)] <- 0
+
 ts_data_sum$DATE <- as.Date(ts_data_sum$DATE, format = "%m/%d/%Y")
 
-ts_plot_title1 <- paste0("Motor Crashes: ", min(ts_data_sum$DATE), " to ", max(ts_data_sum$DATE))
+ts_plot_title1 <- paste0("Motor Crashes, Deaths, Injuries: ", min(ts_data_sum$DATE), " to ", max(ts_data_sum$DATE))
 
 fig <- plotly::plot_ly(
   data=ts_data_sum,
   x=~DATE,
   y=~CRASHES) %>%
   plotly::add_lines() %>%
+  add_trace(x = ~DATE, y = ~KILLED, name = 'KILLED')%>%
+  add_trace(x = ~DATE, y = ~INJURED, name = 'INJURED')%>%
   layout(
     title=ts_plot_title1
   ) %>% 
@@ -98,20 +120,26 @@ print(fig)
 
 saveRDS(fig, "/home/rstudio/motor_crashes/Output/ts_crashes.rds")
 
+# - Pivot table based on car type
+pivot_data <- motor_data[, c("VEHICLE.TYPE.CODE.1", "BOROUGH")]
+rpivotTable(pivot_data, 
+            rows = c("VEHICLE.TYPE.CODE.1"))
+
+
 ############################################################
 # tests:
 # - hypt tests: before Pandemic, after pandemic - mean test for crash differences
 ############################################################
 
-covid_start <- as.Date("03/12/2020", format = "%m/%d/%Y")
+covid_start <- as.Date("01/10/2020", format = "%m/%d/%Y")
 
 # populations:
 before_covid <- ts_data_sum[as.Date(ts_data_sum$DATE, format = "%m/%d/%Y") < covid_start, ]
 after_covid <- ts_data_sum[as.Date(ts_data_sum$DATE, format = "%m/%d/%Y") >= covid_start, ]
 
 # samples:
-bf_sample1 <- sample(before_covid$CRASHES, size = 100)
-aft_sample1 <- sample(after_covid$CRASHES, size = 100)
+bf_sample1 <- sample(before_covid$CRASHES, size = 200)
+aft_sample1 <- sample(after_covid$CRASHES, size = 200)
 
 hist_fig1 <- plot_ly(x = bf_sample1, type = "histogram") %>%
   layout(
@@ -127,12 +155,19 @@ hist_fig2 <- plot_ly(x = aft_sample1, type = "histogram")%>%
     xaxis = list(title = "CRASHES")
   )
 
+
+norm2 <- shapiro.test(bf_sample1)
+
+hyp_res <- t.test(bf_sample1, aft_sample1) 
+
+res <- wilcox.test(aft_sample1, bf_sample1, alternative = "less")
+
+
 saveRDS(hist_fig1, "/home/rstudio/motor_crashes/Output/hist_hyp_before.rds")
-
 saveRDS(hist_fig2, "/home/rstudio/motor_crashes/Output/hist_hyp_after.rds")
+saveRDS(aft_sample1, "/home/rstudio/motor_crashes/Output/hyp_sample_aft.rds")
+saveRDS(bf_sample1, "/home/rstudio/motor_crashes/Output/hyp_sample_bf.rds")
 
-
-t.test(bf_sample1, aft_sample1) 
 
 
 
